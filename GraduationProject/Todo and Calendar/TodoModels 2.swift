c39623f024ac88f95f7e0b4f0c9ce7771e10d9c9
Yun <3430yun>
2023-09-14 21:36:27 +0800

    first

---
+++ GraduationProject/Todo and Calendar/TodoModels 2.swift
@@ -0,0 +1,71 @@
+//
+//  TodoModels.swift
+//  GraduationProject
+//
+//  Created by heonrim on 8/8/23.
+//
+
+import Foundation
+
+struct Task: Identifiable {
+    // 以下是他的屬性
+    var id: Int
+    var label: String
+    var title: String
+    var description: String
+    var nextReviewDate: Date
+    var nextReviewTime: Date
+    var repetition1Count: Date
+    var repetition2Count: Date
+    var repetition3Count: Date
+    var repetition4Count: Date
+    var isReviewChecked0: Bool
+    var isReviewChecked1: Bool
+    var isReviewChecked2: Bool
+    var isReviewChecked3: Bool
+}
+
+
+struct Todo: Identifiable {
+    var id: Int
+    var label: String
+    var title: String
+    var description: String
+    var startDateTime: Date
+    var isRecurring: Bool
+    var recurringOption: Int
+    var selectedFrequency: Int
+    var todoStatus: Bool
+    var dueDateTime: Date
+    var reminderTime: Date
+    var todoNote: String
+}
+
+struct Sport: Identifiable {
+    var id: Int
+    var label: String
+    var title: String
+    var description: String
+    var startDateTime: Date
+    
+    var selectedSport: String
+    var sportValue: Float
+    var sportUnits: String
+    
+    var isRecurring: Bool
+    var recurringOption: Int
+    var selectedFrequency: Int
+    var todoStatus: Bool
+    var dueDateTime: Date
+    var reminderTime: Date
+    var todoNote: String
+}
+
+struct Ticker: Identifiable {
+    var id: String
+//    var ticker_id: String
+    var name: String
+    var deadline: Date
+//    var exchage: Date
+    var exchage: String
+}
